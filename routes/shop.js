655b50bae14c44525a950e1f33da0f514a815567
Artur <arturlee_word@hotmail.com>
2019-10-14 14:12:05 +0200

    asda

--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,16 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/',(req,res,nxt) =>{
+router.get('/', (req, res, nxt) => {
     const products = adminData.products;
-    res.render('shop',{prods: products, pageTitle: 'Shop', path: '/'});
+    res.render('shop', { 
+        prods: products, 
+        pageTitle: 'Shop', 
+        path: '/', 
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
 });
 
 module.exports = router;
