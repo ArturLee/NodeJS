07de9422cf324d84226fdb03a2ac885f83d7cd0f
Artur <arturlee_word@hotmail.com>
2019-10-17 17:52:04 +0200

    Model View Controller

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyparser= require('body-parser');
 //const expressHbs = require('express-handlebars');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 
@@ -12,17 +14,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views','views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminData.routes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,nxt)=>{
-    res.status(404).render('404', {pageTitle: 'Page not found!'});
-});
+app.use(errorController.get404);
 
 app.listen(3000); 
--- routes/admin.js
+++ routes/admin.js
@@ -2,21 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+
 
 // /admin/add-product => GET
-router.get('/add-product',(req,res,nxt) =>{
-    res.render('add-product',{pageTitle: 'Add Product', path: '/admin/add-product', activeProduct:true, formsCSS:true, productCSS:true});//res.sendFile(path.join(rootDir, 'views', 'add-product.html'));///Users/arturlee/Desktop/NodeJS/views/add-product.html
-});
+router.get('/add-product', productController.getAddProduct );
 // /admin/add-product => POST
-router.post('/add-product',(req,res,nxt)=>{
-    products.push({title: req.body.title});
-    res.redirect('/');
-});
+router.post('/add-product', productController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
