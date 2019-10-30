b55f3a1f8d7be57046448c9339008fbeb7ad2c8c
Artur <arturlee_word@hotmail.com>
2019-10-31 00:57:06 +0100

    MVC update

---
+++ controllers/shop.js
@@ -0,0 +1,43 @@
+const Product = require('../models/products');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products',
+      // hasProducts: products.length > 0,
+      // activeShop: true,
+      // productCSS: true
+    });
+  });
+};
+
+exports.getIndex = (req, res, nxt) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      // hasProducts: products.length > 0,
+      // activeShop: true,
+      // productCSS: true
+    });
+  });
+};
+
+exports.getCart = (req, res, nxt) => {
+  res.render('shop/cart', {
+    //prods: products,
+    pageTitle: 'Your Cart',
+    path: '/cart',
+  });
+};
+
+exports.getCheckout = (req,res,nxt) => {
+  res.render('shop/checkout', {
+    //prods: products,
+    pageTitle: 'Checkout',
+    path: '/checkout',
+  });
+}
